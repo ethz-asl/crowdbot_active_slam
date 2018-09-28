e8fcf2400c499520962158c07c1419577af393c0
dmammolo <dmammolo@student.ethz.ch>
2018-09-28 10:39:43 +0200

    add bash file to start general pioneer simulation and SLAM system

---
+++ run_pioneer_SLAM.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Launching pioneer in gazebo and rviz..."
+roslaunch crowdbot_active_slam pioneer_gazebo.launch &
+pid="$pid $!"
+
+sleep 10s
+
+echo "Launching laser scan matcher..."
+roslaunch crowdbot_active_slam scan_matcher.launch &
+pid="$pid $!"
+
+sleep 2s
+
+echo "Launching factor graph SLAM system..."
+roslaunch crowdbot_active_slam graph_optimisation.launch &
+pid="$pid $!"
+
+trap "echo Killing all processes.; kill -2 TERM $pid; exit" SIGINT SIGTERM
+
+sleep 24h
